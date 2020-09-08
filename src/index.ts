d60e479406851c61a04bc14632ef34c902687df8
WatDuhHekBro <44940783+WatDuhHekBro@users.noreply.github.com>
2020-09-08 18:15:20 -0500

    Changed export method

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import * as discord from "./modules/discord";
-import * as util from "./modules/util";
-import * as loader from "./modules/loader";
-import * as permissions from "./modules/permissions";
-import * as dynamic from "./modules/dynamic";
-import * as constants from "./modules/constants";
-import * as command from "./modules/command";
-
-export = {
-	...discord,
-	...util,
-	...loader,
-	...permissions,
-	...dynamic,
-	...constants,
-	...command
-};
+export * from "./modules/discord";
+export * from "./modules/util";
+export * from "./modules/loader";
+export * from "./modules/permissions";
+export * from "./modules/dynamic";
+export * from "./modules/constants";
+export * from "./modules/command";
