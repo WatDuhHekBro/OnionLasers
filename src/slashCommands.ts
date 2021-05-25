2f2d53a7fd64d95451713d6db0c6862fb278c79e
WatDuhHekBro <watduhhekbro@protonmail.com>
2021-05-25 15:32:36 -0500

    Revert "Attempted to carry type info from options"
    This reverts commit 37f100396a8c5ffc56486d965c9210d118580874.

--- src/slashCommands.ts
+++ src/slashCommands.ts
@@ -4,12 +4,7 @@ import {
     CommandInteraction,
     ApplicationCommandData,
     ApplicationCommandOptionData,
-    ApplicationCommandOptionChoice,
-    CommandInteractionOption,
-    User,
-    GuildMember,
-    GuildChannel,
-    Role
+    ApplicationCommandOptionChoice
 } from "discord.js";
 import {slashCommandRegistry} from "./loader";
 import {NO_DESCRIPTION} from "./util";
@@ -24,159 +19,33 @@ export enum SlashCommandOptionType {
     MENTIONABLE = 9
 }
 
-// And thus, I shall commit one of the greatest sins of my time using TypeScript because I don't know a better way to do this.
-// Please have mercy on me. No one should ever have to bear witness to this travesty.
-
 interface RestrictedApplicationCommandOptionBase {
+    type: Exclude<SlashCommandOptionType, SlashCommandOptionType.STRING | SlashCommandOptionType.INTEGER>;
     name: string;
     description?: string;
+    required?: boolean;
 }
-interface RestrictedApplicationCommandOptionString extends RestrictedApplicationCommandOptionBase {
+
+interface RestrictedApplicationCommandOptionString {
     type: SlashCommandOptionType.STRING;
+    name: string;
+    description?: string;
+    required?: boolean;
     choices?: {[name: string]: string};
 }
-interface RestrictedApplicationCommandOptionStringRequired extends RestrictedApplicationCommandOptionString {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionStringOptional extends RestrictedApplicationCommandOptionString {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionInteger extends RestrictedApplicationCommandOptionBase {
+
+interface RestrictedApplicationCommandOptionInteger {
     type: SlashCommandOptionType.INTEGER;
+    name: string;
+    description?: string;
+    required?: boolean;
     choices?: {[name: string]: number};
-}
-interface RestrictedApplicationCommandOptionIntegerRequired extends RestrictedApplicationCommandOptionInteger {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionIntegerOptional extends RestrictedApplicationCommandOptionInteger {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionBoolean extends RestrictedApplicationCommandOptionBase {
-    type: SlashCommandOptionType.BOOLEAN;
-}
-interface RestrictedApplicationCommandOptionBooleanRequired extends RestrictedApplicationCommandOptionBoolean {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionBooleanOptional extends RestrictedApplicationCommandOptionBoolean {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionUser extends RestrictedApplicationCommandOptionBase {
-    type: SlashCommandOptionType.USER;
-}
-interface RestrictedApplicationCommandOptionUserRequired extends RestrictedApplicationCommandOptionUser {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionUserOptional extends RestrictedApplicationCommandOptionUser {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionChannel extends RestrictedApplicationCommandOptionBase {
-    type: SlashCommandOptionType.CHANNEL;
-}
-interface RestrictedApplicationCommandOptionChannelRequired extends RestrictedApplicationCommandOptionChannel {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionChannelOptional extends RestrictedApplicationCommandOptionChannel {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionRole extends RestrictedApplicationCommandOptionBase {
-    type: SlashCommandOptionType.ROLE;
-}
-interface RestrictedApplicationCommandOptionRoleRequired extends RestrictedApplicationCommandOptionRole {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionRoleOptional extends RestrictedApplicationCommandOptionRole {
-    required?: false;
-}
-interface RestrictedApplicationCommandOptionMentionable extends RestrictedApplicationCommandOptionBase {
-    type: SlashCommandOptionType.MENTIONABLE;
-}
-interface RestrictedApplicationCommandOptionMentionableRequired extends RestrictedApplicationCommandOptionMentionable {
-    required: true;
-}
-interface RestrictedApplicationCommandOptionMentionableOptional extends RestrictedApplicationCommandOptionMentionable {
-    required?: false;
 }
 
 type RestrictedApplicationCommandOption =
-    | RestrictedApplicationCommandOptionStringRequired
-    | RestrictedApplicationCommandOptionStringOptional
-    | RestrictedApplicationCommandOptionIntegerRequired
-    | RestrictedApplicationCommandOptionIntegerOptional
-    | RestrictedApplicationCommandOptionBooleanRequired
-    | RestrictedApplicationCommandOptionBooleanOptional
-    | RestrictedApplicationCommandOptionUserRequired
-    | RestrictedApplicationCommandOptionUserOptional
-    | RestrictedApplicationCommandOptionChannelRequired
-    | RestrictedApplicationCommandOptionChannelOptional
-    | RestrictedApplicationCommandOptionRoleRequired
-    | RestrictedApplicationCommandOptionRoleOptional
-    | RestrictedApplicationCommandOptionMentionableRequired
-    | RestrictedApplicationCommandOptionMentionableOptional;
-
-interface UserMemberArgument {
-    user: User;
-    member: GuildMember;
-}
-
-type MentionableArgument = UserMemberArgument | Role;
-
-// Input RestrictedApplicationCommandOption and it'll resolve to <type> or <type | null>.
-// Usage: SlashCommandOptionTypeTypeResolver<typeof thing>
-type SlashCommandOptionTypeTypeResolver<
-    T extends RestrictedApplicationCommandOption
-> = T["type"] extends SlashCommandOptionType.STRING
-    ? T["required"] extends true
-        ? string
-        : string | null
-    : T["type"] extends SlashCommandOptionType.INTEGER
-    ? T["required"] extends true
-        ? number
-        : number | null
-    : T["type"] extends SlashCommandOptionType.BOOLEAN
-    ? T["required"] extends true
-        ? boolean
-        : boolean | null
-    : T["type"] extends SlashCommandOptionType.USER
-    ? T["required"] extends true
-        ? UserMemberArgument
-        : UserMemberArgument | null
-    : T["type"] extends SlashCommandOptionType.CHANNEL
-    ? T["required"] extends true
-        ? GuildChannel
-        : GuildChannel | null
-    : T["type"] extends SlashCommandOptionType.ROLE
-    ? T["required"] extends true
-        ? Role
-        : Role | null
-    : T["type"] extends SlashCommandOptionType.MENTIONABLE
-    ? T["required"] extends true
-        ? MentionableArgument
-        : MentionableArgument | null
-    : null;
-
-// Input RestrictedApplicationCommandOption[] and it'll resolve to <[boolean, string | null]> for example.
-type SlashCommandOptionTypeTupleTypeResolver<
-    T extends [RestrictedApplicationCommandOption, RestrictedApplicationCommandOption]
-> = [T[0], T[1]];
-
-// This should be the end of that abomination's code segment.
-// Sure, I could do "type: Exclude<SlashCommandOptionType, SlashCommandOptionType.STRING | SlashCommandOptionType.INTEGER>".
-// The problem is, I don't know how to extract the types of an actual object.
-// Doing "typeof thing" would only bring up SlashCommandOptionType instead of the actual value.
-// On top of that, "required" changes whether or not the value is null, but I don't know how to do that in a succinct way either.
-
-const a: [RestrictedApplicationCommandOption, RestrictedApplicationCommandOption] = [
-    {
-        type: SlashCommandOptionType.BOOLEAN,
-        name: "nop",
-        required: true
-    },
-    {
-        type: SlashCommandOptionType.STRING,
-        name: "nop"
-    }
-];
-type test = SlashCommandOptionTypeTupleTypeResolver<typeof a>;
+    | RestrictedApplicationCommandOptionBase
+    | RestrictedApplicationCommandOptionString
+    | RestrictedApplicationCommandOptionInteger;
 
 interface SlashCommandOptionsRootBase {
     readonly description?: string;
@@ -273,34 +142,7 @@ export class SlashCommand {
         };
     }
 
-    public async execute(interaction: CommandInteraction): Promise<void> {
-        let options: CommandInteractionOption[] = interaction.options;
-
-        // Don't waste any lines of code on checking whether or not the option type is valid.
-        // If these safety guarantees aren't actually there, then the code needs fixing.
-        // Types of SUBCOMMAND_GROUP and SUBCOMMAND will be guaranteed to have exactly one element.
-        if (this.data.subcommands) {
-            const suboption = interaction.options[0];
-            const header = suboption.name;
-            const subcommand = this.data.subcommands[header];
-
-            if (subcommand.subcommands) {
-                const subsuboption = suboption.options![0];
-                const subheader = subsuboption.name;
-                const subsubcommand = subcommand.subcommands[subheader];
-                options = subsuboption.options || [];
-                console.debug(1, header, subheader, options);
-                subsubcommand;
-            } else {
-                options = suboption.options || [];
-                console.debug(2, header, options);
-                subcommand;
-            }
-        } else {
-            console.debug(4, options);
-            this.data;
-        }
-
+    public async execute(interaction: CommandInteraction) {
         interaction.reply("Soon:tm:");
     }
 
@@ -421,10 +263,7 @@ export function attachSlashCommandHandlerToClient(client: Client, devServers: st
                 }
             } else {
                 interaction.reply(
-                    "Sorry, this command didn't match anything in the registry. This might have been caused by the command list failing to sync up.\n\nKeep in mind that there's a delay for up to an hour when loading global commands while guild commands update instantly.",
-                    {
-                        ephemeral: true
-                    }
+                    "Sorry, this command didn't match anything (add more text here about potential codebase error or outdated slash commands)"
                 );
             }
         }
